7e6a1324332ed1129626bcab66180e13459807d8
MML Tech <11877277+mmlTools@users.noreply.github.com>
2025-01-07 20:07:57 +0200

    main publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GoogleFonts} from "./global-data";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {SelectModule} from "primeng/select";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    FloatLabelModule,
+    SelectModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-font-picker';
+export class AppComponent implements OnInit{
+  filteredFonts: any = [];
+  loadedFonts: string[] = [];
+  baseFont: any;
+
+  ngOnInit(): void {
+    this.baseFont = {name: 'ABeeZee'};
+    this.filterFonts({filter: ""});
+  }
+
+  filterFonts(event: any): void {
+    const query = event.filter.toLowerCase();
+    this.filteredFonts = GoogleFonts
+        .filter(fn => fn.toLowerCase().includes(query))
+        .slice(0, 10)
+        .map(fn => ({ name: fn }));
+    this.filteredFonts.forEach((font: any) => this.loadFont(font.name));
+  }
+
+  loadFont(fontName: string): void {
+    const fontFamily = fontName.replace(/ /g, '+');
+    const encodedFont = btoa(fontName.toLowerCase());
+
+    if (!this.loadedFonts.includes(encodedFont)) {
+      const fontLink = document.createElement('link');
+      fontLink.rel = 'stylesheet';
+      fontLink.href = `https://fonts.googleapis.com/css?family=${fontFamily}:100,200,300,400,500,600,700,800,900`;
+      document.head.appendChild(fontLink);
+
+      this.loadedFonts.push(encodedFont);
+    }
+  }
 }
